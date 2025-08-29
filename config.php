e4d1c9b506380c8ae68a455ce5f4a6688b3090e9
Marinda <macbookpro@192.168.1.66>
2025-08-29 13:01:46 +0200

    some push

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 // Database configuration
 $host = "localhost";
 $user = "root"; // Default XAMPP username
-$pass = " ";     // Default XAMPP password is empty
+$pass = "";     // Default XAMPP password is empty
 // $pass = "teller";     // Default XAMPP password is empty
 $dbname = "patronix_db";
 
